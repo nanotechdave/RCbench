5a8f4b534b2fcfc1abde40dae707a6a961478073
nanotechdave <davide.pilati@gmail.com>
2025-04-07 15:35:35 +0200

    test_release init

--- rcbench/__init__.py
+++ rcbench/__init__.py
@@ -4,6 +4,9 @@ from .utils import utils
 from .tasks.nlt import NltEvaluator
 from .tasks.memorycapacity import MemoryCapacityEvaluator
 from .tasks.sinx import SinxEvaluator
+from .tasks.kernelrank import KernelRankEvaluator
+from .tasks.generalizationrank import GeneralizationRankEvaluator
+from .tasks.narma import NarmaEvaluator
 from .measurements.parser import MeasurementParser
 from .measurements.loader import MeasurementLoader
 from .measurements.dataset import ReservoirDataset

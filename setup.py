78c9d9f8888b7a3b9872ff6a258b5b025a29dbde
nanotechdave <davide.pilati@gmail.com>
2025-04-07 10:39:16 +0200

    gitactions

--- setup.py
+++ setup.py
@@ -25,6 +25,9 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         "Operating System :: OS Independent",
     ],
     install_requires=[

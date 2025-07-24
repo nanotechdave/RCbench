bf50218f02d3c771260c082443bbd4a79a34c32c
Davide Pilati <104424152+nanotechdave@users.noreply.github.com>
2025-07-24 21:40:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="rcbench",
-    version="0.1.0",
+    version="0.1.10.dev1",
     description="Reservoir computing benchmark toolkit",
     #package_dir={"":"rcbench"},
     packages=find_packages(),
